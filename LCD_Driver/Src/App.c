22cb8d2a6d4c794cc2584060bc6162db479f6b90
AbdalrahmanHelal <abdlrhmnhelal@gmail.com>
2024-08-08 16:48:35 +0300

    updated LCD_Driver and added Keypad_Driver

--- LCD_Driver/Src/App.c
+++ LCD_Driver/Src/App.c
@@ -10,6 +10,7 @@
 #include <util/delay.h>
 
 
+
 #include "../Include/MCAL/DIO/DIO_Interface.h"
 #include "../Include/HAL/LCD/LCD_interface.h"
 int main(void)
@@ -18,7 +19,19 @@ int main(void)
 	MDIO_voidInit();
 	HLCD_voidInit();
 
-	HLCD_voidDisplayString("7pepy bo7meed");
+	u8 customChar[8] = {
+	  0b10101,
+	  0b10101,
+	  0b10101,
+	  0b10101,
+	  0b11111,
+	  0b10101,
+	  0b01110,
+	  0b10101
+	};
+
+	HLCD_voidDisplayString("3bdo");
+	HLCD_voidSendSpecialSymbol(customChar,LCD_PATTERN0,LCD_ROW2,LCD_COL7);
 	while(1)
 	{
 
